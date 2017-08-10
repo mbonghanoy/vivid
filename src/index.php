6a746f0760e46a0ce25a421172cf18b01d75791d
mbonghanoy <bonghanoymark@gmail.com>
2017-08-10 10:19:27 +0800

    added some changes in edit function

--- src/index.php
+++ src/index.php
@@ -94,14 +94,15 @@ class Vivid
             $key = implode(",", $keys);
             $values = array_values($data);
             $value = implode("','", $values);
-            $sql = "INSERT INTO $this->table($key) VALUES (?)";
+            $sql = "INSERT INTO $this->table($key) VALUES ('$value')";
             $query = $this->conn->prepare($sql);
-            $query->execute(array($data));
+            $query->execute();
             $this->results = $query->fetchAll(PDO::FETCH_OBJ);
         }catch (PDOException $ex) {
             echo $ex->getMessage();
         }
     }
+
     public function edit($newInput = [], $user_id)
     {
         try{
@@ -118,9 +119,9 @@ class Vivid
 }
 
 $connection = new Vivid('localhost', 'root', 'password', 'phonebook');
-$newInput = [
+$data = [
     'first_name'=>'jMark',
     'last_name'=>'hey',
 ];
 $user = $connection->table('user')
-    ->edit($newInput, '1');
+    ->insert($data);
